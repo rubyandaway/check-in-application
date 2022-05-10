bd2f85ea57028110fc8fcd4b7b2b95de85dbfc8b
bomafiber <bomafiber@yahoo.co.uk>
2022-05-10 12:10:22 +0100

    removed commented modules

--- main.tf
+++ main.tf
@@ -17,30 +17,6 @@ locals {
   cluster_name = "check-in-cluster"
 }
 
-#module "vpc" {
-#  source  = "terraform-aws-modules/vpc/aws"
-#  version = "2.47.0"
-#
-#  name                 = "k8s-vpc"
-#  cidr                 = "10.16.0.0/16"
-#  azs                  = data.aws_availability_zones.available.names
-#  private_subnets      = ["10.16.1.0/24", "10.16.2.0/24", "10.16.3.0/24"]
-#  public_subnets       = ["10.16.4.0/24", "10.16.5.0/24", "10.16.6.0/24"]
-#  enable_nat_gateway   = true
-#  single_nat_gateway   = true
-#  enable_dns_hostnames = true
-#
-#  public_subnet_tags = {
-#    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#    "kubernetes.io/role/elb"                      = "1"
-#  }
-#
-#  private_subnet_tags = {
-#    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-#    "kubernetes.io/role/internal-elb"             = "1"
-#  }
-#}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -61,47 +37,6 @@ module "vpc" {
   }
 }
 
-#module "eks" {
-#  source  = "terraform-aws-modules/eks/aws"
-#  version = "18.0"
-#
-#  cluster_name    = local.cluster_name
-#  cluster_version = "1.17"
-#  subnets         = module.vpc.private_subnets
-#
-#  vpc_id = module.vpc.vpc_id
-#
-#  node_groups = {
-#        public = {
-#          subnets          = [module.vpc.public_subnets]
-#          desired_capacity = 1
-#          max_capacity     = 10
-#          min_capacity     = 1
-#
-#          instance_type = "t2.small"
-#          k8s_labels = {
-#            Environment = "public"
-#          }
-#        }
-#        private = {
-#          subnets          = [module.vpc.private_subnets]
-#          desired_capacity = 1
-#          max_capacity     = 10
-#          min_capacity     = 1
-#
-#          instance_type = "t2.small"
-#
-#          k8s_labels = {
-#            Environment = "private"
-#          }
-#        }
-#
-#  }
-#
-##  write_kubeconfig   = true
-##  config_output_path = "./"
-#}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
@@ -120,14 +55,12 @@ module "eks" {
       name                = "worker-group-1"
       instance_type       = "t2.small"
       additional_userdata = "echo foo bar"
-      #      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity = 2
     },
     {
       name                = "worker-group-2"
       instance_type       = "t2.medium"
       additional_userdata = "echo foo bar"
-      #      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity = 1
     },
   ]
